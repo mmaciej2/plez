7d5ac05a7a4e19d733aca9fb3b74b1d510a3805b
Matt Maciejewski <matt@mmaciejewski.com>
2025-01-15 17:09:16 -0500

    external mplstyle file

--- src/plez/core.py
+++ src/plez/core.py
@@ -1,11 +1,11 @@
+from importlib.resources import files
+
 import matplotlib
 matplotlib.use('pgf')
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pgf import PdfPages
 
 plt.rcParams.update({
-    "font.family": "serif",
-    "font.serif": ["Times"],
     "text.usetex": True,
     "pgf.texsystem": "pdflatex",
     "pgf.rcfonts": False,
@@ -14,21 +14,11 @@ plt.rcParams.update({
          r"\usepackage{amsmath}",
          r"\usepackage{amssymb}",
     ]),
-    "xtick.direction": "in",
-    "xtick.labelsize": 9,
-    "ytick.direction": "in",
-    "ytick.labelsize": 9,
-    "axes.labelsize": 9,
-    "axes.titlesize": 9,
-    "figure.labelsize": 9,
-    "figure.titlesize": 9,
-    "font.size": 9,
-    "legend.fontsize": 9,
-    "legend.title_fontsize": 9,
 })
 
 class Plotter:
     def __init__(self, filename):
+        plt.style.use(files("plez.mplstyle") / "IEEE.mplstyle")
         self.filename = filename
 
     def __enter__(self):
