3c00ee9d6e9d40d016995526b2c253e9bc8ca945
Matt Maciejewski <matt@mmaciejewski.com>
2025-01-22 18:01:58 -0500

    updated for multiple use cases

--- plez/core.py
+++ plez/core.py
@@ -1,4 +1,5 @@
 from importlib.resources import files
+from contextlib import contextmanager
 
 import matplotlib
 matplotlib.use('pgf')
@@ -7,7 +8,8 @@ from matplotlib.backends.backend_pgf import PdfPages
 
 class Plotter:
     def __init__(self,
-            filename,
+            *args,
+            filename=None,
             height=1.0,
             height_unit="ratio",
             col_span=1,
@@ -19,34 +21,62 @@ class Plotter:
             texsystem="pdflatex",
         ):
 
-        self.filename = filename
+        assert len(args) <= 1
+        try:
+            self.filename = args[0]
+        except IndexError:
+            self.filename = filename
 
-        # Compute dimensions of figure
+        self._figsize = self.parse_plot_configs(height, height_unit, col_span, scale)
+
+        self._style = style
+        self._rcfonts = rcfonts
+        self._preamble = preamble
+        self._extra_preamble = extra_preamble
+        self._texsystem = texsystem
+
+        self.plt = plt
+
+    def parse_plot_configs(self, height, height_unit, col_span, scale):
         assert height_unit in ["ratio", "in", "cm"], 'Only "ratio", "in", and "cm" are valid units'
         assert col_span in list(range(1, len(self.widths)+1)), "Invalid number of columns for template"
         width = self.widths[col_span-1] * scale
         if height_unit == "ratio":
-            self.figsize = (width, width*height)
+            return (width, width*height)
         elif height_unit == "in":
-            self.figsize = (width, height)
+            return (width, height)
         elif height_unit == "cm":
-            self.figsize = (width, height*CM)
+            return (width, height/2.54)
 
-        # Configure the style
-        plt.style.use(files("plez.mplstyle") / style)
-        plt.rcParams.update({
-            "pgf.rcfonts": rcfonts,
-            "pgf.preamble": "\n".join(preamble + extra_preamble),
-            "pgf.texsystem": texsystem,
+    def configure_backend(self):
+        self.plt.style.use(files("plez.mplstyle") / self._style)
+        self.plt.rcParams.update({
+            "pgf.rcfonts": self._rcfonts,
+            "pgf.preamble": "\n".join(self._preamble + self._extra_preamble),
+            "pgf.texsystem": self._texsystem,
         })
 
-        self.plt = plt
-
     def __enter__(self):
-        self.fig = self.plt.figure(figsize=self.figsize)
+        self.configure_backend()
+        self.fig = self.plt.figure(figsize=self._figsize)
         self.pdf = PdfPages(self.filename)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.pdf.savefig()
         self.pdf.close()
+
+    @contextmanager
+    def __call__(self, filename, height=1.0, height_unit="ratio", col_span=1, scale=1.0):
+        figsize = self.parse_plot_configs(height, height_unit, col_span, scale)
+        self.configure_backend()
+        class PDF_Figure:
+            plt = self.plt
+            fig = self.plt.figure(figsize=figsize)
+            pdf = PdfPages(filename)
+        pdf_figure = PDF_Figure()
+        try:
+            yield pdf_figure
+        finally:
+            pdf_figure.pdf.savefig()
+            pdf_figure.pdf.close()
